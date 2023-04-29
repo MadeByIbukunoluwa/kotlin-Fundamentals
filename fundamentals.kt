b860600645581d4616e1afc952da0ed9ec9f81d1
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-04-29 04:52:31 +0100

    Update:modified fundamentals.kt added numbers, type inference

--- fundamentals.kt
+++ fundamentals.kt
@@ -1,6 +1,7 @@
 
 // https://developer.android.com/courses/kotlin-bootcamp/overview
-
+//Kotlin Apprentice 
+import kotlin.math.* 
 
 fun main() {
     println("Hello,World")
@@ -39,6 +40,129 @@ fun main() {
     println(1 shl 3 )
     println(32 shr 2)
     // On reason for using shifts is making multiplting or dividing by powers of two easy 
-    // Shifting left by one is the same as multiplying by two, shifiting ledt by two is the same thing as multiplying by four and so on....
+    // Shifting left by one is the same as multiplying by two, shifiting left by two is the same thing as multiplying by four and so on....
+
+    //Operator Precedence in Kotlin 
+    350 / 5 + 2 
+    // dvides by 5 then adds 2 
+    350 / (5 + 2)
+    // evaluates 5 + 2 then 350 divides it 
+
+    //Math functions 
+    println(sin(45 * PI / 180));
+
+    println(sin(135 * PI / 180));
+
+    println(sqrt(7.9));
+
+    println(max(8, 137));
+
+    min(-3 ,-14);
+
+    //Naming data 
+    // You use the val keyword to declare constants , it acnnot be reassigned 
+    val number : Int = 10
+    // The IDE will show an error 
+    // number = 14;
+    val pi : Double = 3.14159
+
+    // you can also add the const keyword to val to make it a compile time constant 
     
-}
+
+// when you have data that might cange over time you declare it with a variale with var 
+
+    var variableNumber = 0
+
+
+    variableNumber = 0
+    variableNumber = 1_000_000
+
+    // increment and decrement
+    var counter : Int = 0;
+
+    counter += 1 
+    
+    counter -= 1 
+
+    //same thing as 
+    var counter1 :Int = 0;
+
+    counter1 = counter1 + 1 
+
+    counter1 = counter - 1 
+
+    //Mini Exercises 
+
+    //Challenges 
+
+
+    //Types and operations 
+    // A type represents a set of values adn the operaions that can be performed on them 
+
+    //Type Conversion
+    var integer:Int = 100
+    var decimal:Double = 12.5
+    // integer = decimal 
+
+    //You will get an error - a type mismatch 
+    // Some programmng languages are not as strict and will allow the type conversin , koltin willl not allow you to do that 
+    // instead of assigning , you need to say you want to explicitly convert the tyep 
+
+    // you now tell kotlin you want to convert fro the original type Double to the new type Int 
+
+    // also, assigning a deicmal value to the integer results in a loss of precision 
+
+
+    integer = decimal.toInt()
+
+    print(integer)
+
+    //Operators with mixed types 
+
+    var hourlyRate:Double = 19.5
+
+    var hoursWorked:Int = 10 
+
+    //hours worked is converted to a double here,
+    var totalCost : Double = hourlyRate * hoursWorked.toDouble();
+
+    // but the conversion is unnecessary , kotlin will alow you to multiply these values wihtout any conversion 
+
+    val totalCost1: Double = hourlyRate * hoursWorked
+
+
+    //Sometimes you don't need to tell kotlin the type verytime , by type inference the compile knows 
+    var typeInferredInt = 90
+
+    // Here kotlin infers the type as an int , what if you want it to be inferred as a double 
+    var wantADouble = 3.toDouble();
+
+    // something you may be tempted to try ( and something i though could work is this)
+    //this is not allowed
+    //The integer literal does not conform to the expected type Doublekotlin(CONSTANT_EXPECTED_TYPE_MISMATCH)
+    // var actuallyDouble : Double = 3 ;
+
+    
+} 
+//If we put it inside the main method or inside a class, it will give the error -  Modifier 'const' is not applicable to 'local variable'
+
+
+const val reallyConstant : Int = 42 
+
+
+// Variables 
+
+var variableNumber : Int = 42 ;
+
+// You declare constants using val, while you declare variables using var 
+
+//once you have declared a variables , you can change it however you wish you simply reassign its value 
+
+
+variableNumber = 0
+
+variableNumber = 1_000_000
+
+// in kotlin, you can use underscores to amke larger numbers more human readable 
+
+
